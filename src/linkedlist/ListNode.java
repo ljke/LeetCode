63c2f10cc563321a30acca42699fb6b5181bab7b
ljke <ljke1995@gmail.com>
2019-07-14 15:39:04 +0800

    change ListNode to public

--- src/linkedlist/ListNode.java
+++ src/linkedlist/ListNode.java
@@ -7,11 +7,11 @@ public class ListNode {
     public int val;
     public ListNode next;
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next) {
+    public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
